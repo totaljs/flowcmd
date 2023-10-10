73c35aba3b90d138c21e4e3250e784c7f047f298
Peter Širka <petersirka@gmail.com>
2023-10-10 15:01:38 +0200

    First commit.

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Flow as a command-line tool
+
+The Flow will be executed as a command-line app. It can work on the server.
+
+__Requirements__:
+
+- Node.js +v16
+- NPM
+- Total v4
+
+__Installation__:
+
+- `$ npm install`
+
+__How to run it?__
+
+- `$ npm start`
+
+## How to edit it?
+
+We use Total.js Flow Cloud Designer. Please use the designer only for the required time, don't keep the connection online for a longer time in the background.
+
+- `$ npm start edit`
---
+++ run.js
@@ -0,0 +1,27 @@
+require('total4');
+require('total4/flow');
+
+Flow.on('load', function(instance, meta) {
+
+	console.log('Loaded FlowStream:', meta.name);
+
+	if (instance.$client)
+		instance.$client.destroy();
+
+	var editable = process.argv.includes('--edit') || process.argv.includes('edit') || process.argv.includes('debug');
+
+	editable && WEBSOCKETCLIENT(function(client) {
+		instance.$client = client;
+		var socket = 'wss://flow.totaljs.com/{0}-{1}/'.format(UID());
+		client.connect(socket);
+		FlowStream.client('timer', client);
+		console.log('Link to designer:', 'https://flow.totaljs.com?socket=' + encodeURIComponent(socket));
+	});
+
+});
+
+Flow.on('save', function(data) {
+	F.Fs.writeFile(PATH.flowstreams(data.id + '.flow'), JSON.stringify(data, null, '\t'), ERROR('FlowStream.save'));
+});
+
+Flow.init();
